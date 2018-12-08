39ef1c10a224490bc7b4cb0cd18037de9603f463
Mingyuan ZHOU <sczmy21@gmail.com>
2018-12-08 20:27:48 +0100

    Listing 2.3

---
+++ L2.3_getinfo.cpp
@@ -0,0 +1,14 @@
+//getinfo.cpp -- input and output
+#include<iostream>
+
+int main() {
+	using namespace std;
+
+	int carrrots;
+	cout << "How many carrots do you have?" << endl;
+	cin >> carrrots;
+	cout << "Here are two more. ";
+	carrrots = carrrots + 2;
+	cout << "Now you have " << carrrots << " carrots." << endl;
+	return 0;
+}
