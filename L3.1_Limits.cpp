3323a64be4d70f54b98462d7026e68ad1157ba3a
Mingy2018 <605329720@qq.com>
2018-12-21 21:34:33 +0100

    Review of Integer Type

--- L3.1_Limits.cpp
+++ L3.1_Limits.cpp
@@ -12,6 +12,17 @@ int main()
 	//sizeof operator yields size of type or of variable
 	cout << "int is" << sizeof(int) << " bytes." << endl;
 	cout << "short is" << sizeof(short) << " bytes." << endl;
-	cout<<
+	cout << "long is" << sizeof(long) << " bytes." << endl;
+	cout << "long long is" << sizeof(long long) << " bytes" << endl;
+	cout << endl;
 
+	cout << "Maximum values:" << endl;
+	cout << "int: " << n_int<<endl;
+	cout << "short: " <<n_short<< endl;
+	cout << "long: " << n_long << endl;
+	cout << "long long: " << n_llong << endl;
+
+	cout << "Minimum int value = " << INT_MIN << endl;
+	cout << "Bits per byte = " << CHAR_BIT << endl;
+	return 0;
 }
