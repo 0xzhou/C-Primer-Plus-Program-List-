79d91d32f8b40fdc3f0ac78f5e2d3bb1b468e710
Mingy2018 <39735862+Mingy2018@users.noreply.github.com>
2018-12-28 21:50:36 +0100

    Update L8.2_firstref.cpp

--- L8.2_firstref.cpp
+++ L8.2_firstref.cpp
@@ -6,12 +6,15 @@ int main() {
 	int & rodents = rats;//rodents is a reference
 	cout << "rats = " << rats;
 	cout << ", rodents = " << rodents<<endl;
-	rodents++;
+	rodents++;// Incrementing rodents by one affects both variables.
 	cout << "rats = " << rats;
 	cout << ", rodents = " << rodents<<endl;
 
+	// some implementations require type casting the following
+	// address to type unsigned
 	cout << "rats address = " << &rats;
 	cout << ", rodents address = " << &rodents << endl;
 	return 0;
 	//the reference has the same value and address with it's varible
 }
+// As you can see, both rats and rodents have the same value and the same address.
