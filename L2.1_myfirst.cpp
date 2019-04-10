ba99f8510443bf47fd9d250f12885a089c6c46c3
Mingy2018 <39735862+Mingy2018@users.noreply.github.com>
2019-04-11 00:33:07 +0200

    Update L2.1_myfirst.cpp

--- L2.1_myfirst.cpp
+++ L2.1_myfirst.cpp
@@ -6,5 +6,6 @@ int main() {//function header
 	cout << "Come up and C++ me some time.";//message
 	cout << endl;//start a nem line
 	cout << "You won't regret it!" << endl;//more output
+	cin.get(); // to look at the output on screen
 	return 0;//terminate main()
 }//end of function body~
