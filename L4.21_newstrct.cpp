e03472d166a9894443b0c5fede2025ccf23f3381
Mingy2018 <39735862+Mingy2018@users.noreply.github.com>
2019-04-25 15:20:05 +0200

    Update L4.21_newstrct.cpp

--- L4.21_newstrct.cpp
+++ L4.21_newstrct.cpp
@@ -11,9 +11,9 @@ int main()
 	using namespace std;
 	inflatable * ps = new inflatable;// allot memory for structure
 	cout << "Enter name of inflatable item: ";
-	cin.get(ps->name, 20);
+	cin.get(ps->name, 20); // method 1 for member access
 	cout << "Enter volume in cubic feet: ";
-	cin >> (*ps).volume;
+	cin >> (*ps).volume;// method 2 for member access
 	cout << "Enter price: $";
 	cin >> ps->price;
 	cout << "Name: " << (*ps).name << endl;
