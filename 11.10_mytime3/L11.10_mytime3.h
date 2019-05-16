0e0cd51d128d1b8fdb3dae7e85fe17406f3bdf1d
Mingy2018 <605329720@qq.com>
2019-05-16 13:29:08 +0200

    Finish of Friends

--- 11.10_mytime3/L11.10_mytime3.h
+++ 11.10_mytime3/L11.10_mytime3.h
@@ -18,7 +18,7 @@ public:
 	Time operator+(const Time & t) const;
 	Time operator-(const Time & t) const;
 	Time operator*(double n) const;
-	friend Time operator*(double m, const Time &)
+	friend Time operator*(double m, const Time & t)
 	{
 		return t * m;
 	}
